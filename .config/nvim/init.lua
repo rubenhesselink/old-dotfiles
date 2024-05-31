debe2dee9c1de56c9436354fd2aba393fccadd71
Ruben Hesselink <ruben@fourdigits.nl>
2024-05-31 08:53:10 +0200

    Add noice.nvim to Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -358,6 +358,27 @@ require("lazy").setup({
 			-- 	end, { desc = "Open harpoon window" })
 		end,
 	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so the cmp and other plugins use Treesitter
+					override = {
+						["vim.lsp.util.convert_input_to_markdown"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				presets = {},
+			})
+		end,
+	},
 
 	{ -- LSP Configuration & Plugins
 		"neovim/nvim-lspconfig",
